a13ac47e94bac9f4fda34e8babfcb61e2903dac6
developer.amit14jan@gmail.com <developer.amit14jan@gmail.com>
2021-04-14 00:42:52 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+      public static void main (String [] args){
+             int x=2;
+             int y= 3;
+             if(x>y){
+                System.out.println("Greater Number =" +" " + x);
+             }else{
+                System.out.println("Greater Number" + " "+ y);
+}
+}
+}
